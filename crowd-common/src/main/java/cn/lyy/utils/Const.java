69a33f6d2dd0303909a788ac98430bffbb37ad9f
A18072 <smileliyuyan@163.com>
2019-08-20 16:09:12 +0800

    3.访问权限拦截器-改善(监听器) (2019-6-18 14-29-19)

--- crowd-common/src/main/java/cn/lyy/utils/Const.java
+++ crowd-common/src/main/java/cn/lyy/utils/Const.java
@@ -5,4 +5,5 @@ public class Const {
     public static final String LOGIN_USER = "user";
     public static final String PSSWORD = "123";
     public static final String MY_URLS = "myURIs";
+    public static final String ALL_PERMISSION_URI = "allPermissionUris";
 }
--- crowding-manager-main/src/main/java/cn/lyy/crowdfunding/listener/StartSystemListener.java
+++ crowding-manager-main/src/main/java/cn/lyy/crowdfunding/listener/StartSystemListener.java
@@ -1,8 +1,18 @@
 package cn.lyy.crowdfunding.listener;
 
+import cn.lyy.crowdfunding.bean.Permission;
+import cn.lyy.crowdfunding.manager.service.PermissionService;
+import cn.lyy.utils.Const;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class StartSystemListener implements ServletContextListener {
     @Override
@@ -11,6 +21,21 @@ public class StartSystemListener implements ServletContextListener {
         String contextPath = servletContext.getContextPath();
         servletContext.setAttribute("APP_PATH", contextPath);
         System.out.println("APP_PATH...." + contextPath);
+
+        //2.加载所有许可路径
+
+        ApplicationContext ioc = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+        PermissionService permissionService = ioc.getBean(PermissionService.class);
+        List<Permission> queryAllpermission = permissionService.queryAllPermission();
+
+        Set<String> allURIs = new HashSet<>();
+
+        for (Permission permission : queryAllpermission) {
+            allURIs.add("/" + permission.getUrl());
+        }
+
+        servletContext.setAttribute(Const.ALL_PERMISSION_URI, allURIs);
     }
 
     @Override
